2cb9459815a8ec6acd81f8f7e0f66f964c0c1863
aadarsh-2004 <2020aadarshsoni@gmail.com>
2025-06-01 09:06:56 +0530

    Arthmetic Operations

---
+++ arithmetic_cal.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Multiplication operation
+x=10
+y=20
+let mul=$x*$y
+echo "multiplication of $x and $y is  $mul"
+
+#Sum Opearation
+
+let sum=$x+$y
+echo "Sum of $x and $y is $sum"
+
+#Subtraction Operations
+
+echo "subtractions is $(($x-$y))"
