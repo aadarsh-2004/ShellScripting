f9ca2e93fb578533513b263ff04d21e34977077e
aadarsh-2004 <2020aadarshsoni@gmail.com>
2025-06-01 09:45:02 +0530

    loop

---
+++ loops.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for i in 1 2 3 4 5 
+do
+	echo "number is $i"
+done
+
+for name in Raju Shyam Ram
+do
+	echo "Name is : $name"
+done
+
+for j in {1..50}
+do
+	echo "number is $j"
+done
+
